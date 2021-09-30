cfdc823808ba4a30c67ee572c9a3b07b7a85f2ac
swc <906618379@qq.com>
2021-09-30 11:31:48 +0800

    update 可能wifi经常断线，浏览器dns报错

--- phy.c
+++ phy.c
@@ -2527,7 +2527,7 @@ void rtw89_phy_set_bss_color(struct rtw89_dev *rtwdev, struct ieee80211_vif *vif
 	if (!vif->bss_conf.he_support || !vif->bss_conf.assoc)
 		return;
 
-	bss_color = vif->bss_conf.he_bss_color.color;
+	// bss_color = vif->bss_conf.he_bss_color.color;
 
 	rtw89_phy_write32_idx(rtwdev, R_BSS_CLR_MAP, B_BSS_CLR_MAP_VLD0, 0x1,
 			      phy_idx);
